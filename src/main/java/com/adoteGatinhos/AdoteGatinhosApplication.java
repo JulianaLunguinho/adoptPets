2705ad2da19d3faa639ee1446d6b8916adec18d1
Spina <juliana.s.lunguinho@hotmail.com>
2021-07-09 16:24:18 -0300

    first commit

---
+++ src/main/java/com/adoteGatinhos/AdoteGatinhosApplication.java
@@ -0,0 +1,13 @@
+package com.adoteGatinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteGatinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteGatinhosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adoteGatinhos/AdoteGatinhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoteGatinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteGatinhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
